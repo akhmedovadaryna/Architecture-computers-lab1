af4ae294b324ae187a63d50092818a5e015978ca
IvanovAlexandr <ivanov.alexandr.1995@gmail.com>
2016-03-14 20:35:11 +0200

    "Edit something"

--- Controller.py
+++ Controller.py
@@ -1,45 +1,64 @@
 __author__ = 'alexandr'
 
 import Business_logic as bl
+import View as vw
 
-#вывод приветствия
+# вывод приветствия
 print("PHONEBOOK v1.0.3")
 
-bl.get_phonebook()
+bl.init_phonebook()
 
+def menu():
 
-while(1):
-    print(" MENU ")
+    print("\n MENU ")
     print("Press 1 if you want see phonebook")
     print("Press 2 if you want add new record to phonebook")
     print("Press 3 if you want delete something from phonebook")
     print("Press 4 if you want edit something in phonebook")
     print("Press 5 if you want find something in phonebook")
     print("Press 0 if you want exit")
-    choise = int(input("Enter your choise :  "))
+    return input("Enter your choise :  ")
 
-    if choise == 0:
+def press_any_key():
+    input("Press Enter to continue...")
+
+while(1):
+
+    choise = menu()
+    if choise == '0':
         break
-    elif choise == 1:
-        #вывод фонебука
-        pass
-    elif choise == 2:
-        #ввод новой записи
+    elif choise == '1':
+        # вывод фонебука
+        vw.view_phonebook(bl.list_with_dict)
+        press_any_key()
+    elif choise == '2':
+        # ввод новой записи
+        new_number, new_FIO, new_street, new_house= vw.read_new_contact()
         bl.add(new_number, new_FIO, new_street, new_house)
-        #вывод добавленной строки
-    elif choise == 3:
-        #выбор что удалять
-        bl.delete(phone_number)
-    elif choise == 4:
-        #выбор строчки для редактирования
-        #ввод новых значений
-        bl.change(key, number, FIO, street, house)
-    elif choise == 5:
-        #ввод критериев поиска
-        bl.find(something)
-    else:
-        #некорректный выбор
+        print("Successfull!")
+        press_any_key()
+    elif choise == '3':
+        # выбор что удалять
+
+        if (bl.delete(vw.del_choice())):
+            print("Successfull!")
+        else:
+            print("Not found!")
+        press_any_key()
+    elif choise == '4':
+        # выбор строчки для редактирования
+        key = vw.edit_choice()
+        # ввод новых значений
+        number, FIO, street, house = vw.new_values()
+        bl.change(key, number, FIO, street, house )
+    elif choise == '5':
         pass
+        # ввод критериев поиска
+      #  vw.what_to_find()
+      #  bl.find(something)
+    else:
+        # некорректный выбор
+        vw.uncorrect()
 
 print("Thank you!")
 
